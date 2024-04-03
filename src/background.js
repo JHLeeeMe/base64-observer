46c231e763b3374b38b9bc34986ba7929f9d73cc
JHLeeeMe <lejung92@gmail.com>
2024-04-04 08:59:14 +0900

    [Update] refactoring
    1. Modify `callback` -> `async/await`

--- src/background.js
+++ src/background.js
@@ -2,19 +2,45 @@
 ///
 ///
 
-chrome.runtime.onInstalled.addListener(async (details) => {
-  if (details.reason == "install") {
-    await chrome.storage.local.set({isActive: false});
+////////////////////////////////////////////////////////////////////////////////
+/// Functions
+////////////////////////////////////////////////////////////////////////////////
+async function onInstalledListener(details) {
+  if (details.reason !== "install") {
+    return;
   }
-});
+
+  try {
+    await chrome.storage.local.set({isActive: false})
+  } catch (e) {
+    console.error("Error onInstalledListener(...):", e);
+  }
+}
 
 /// Shipping messages from popup.js to content-script_start.js
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.from == "popupjs-toggleBtn") {
+///
+async function onMessageListener(request, sender, sendResponse) {
+  if (request.from !== "popupjs-toggleBtn") {
+    return;
+  }
+
+  try {
     const localData = await chrome.storage.local.get("isActive");
     chrome.tabs.sendMessage(
       request.currentTabs[0].id,
       {from: "backgroundjs-popupjs-toggleBtn", enabled: localData.isActive}
     );
+  } catch (e) {
+    console.error("Error onMessageListener(...):", e);
   }
-});
+}
+
+function init() {
+  chrome.runtime.onInstalled.addListener(onInstalledListener);
+  chrome.runtime.onMessage.addListener(onMessageListener);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Main
+////////////////////////////////////////////////////////////////////////////////
+init();
